866b0362cb34f28d365232d5a7a6aacd4a4f39a5
TuNombreDeUsuario <aldairbz1719@gmail.com>
2025-07-04 15:59:08 +0200

    1.0.0
    creado el proyecto angular

---
+++ F.LiveLevelUp/src/app/pages/mundo/mundo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mundo',
+  imports: [],
+  templateUrl: './mundo.html',
+  styleUrl: './mundo.css'
+})
+export class Mundo {
+
+}
